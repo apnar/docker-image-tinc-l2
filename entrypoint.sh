0c3466813ba9e564c0091a7d668f5841373cf4dc
Josh Lukens <jlukens@botch.com>
2017-06-09 21:14:06 -0400

    reduce hardcoded network scripts and added wait ability

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-# convert eth0 to bridge
-GW=$(/sbin/ip route | awk '/default/ { print $3 }')
-brctl addbr tinc-bridge
-ifconfig tinc-bridge `ifconfig eth0 | grep netmask | sed -e 's/.*inet//' -e 's/broad.*//'`
-ifconfig eth0 down
-ip address flush dev eth0
-brctl addif tinc-bridge eth0
-ifconfig eth0 up
-route add default gw $GW
-ip address flush dev eth0
+if [ ! -z "${WAIT_INT}" ]; then
+  /usr/bin/pipework --wait -i ${WAIT_INT}
+fi
 
 exec /usr/sbin/tinc start -D -U nobody
 
